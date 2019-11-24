bc6fbde8276ca1e1d542fcc9838f22c78b0bd617
Erik Tenecela <eriktenecela13@gmail.com>
2019-11-24 13:16:28 -0500

    setup

---
+++ models/example.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+module.exports = function(sequelize, DataTypes) {
+    var Example = sequelize.define("Example", {
+        text: DataTypes.STRING,
+        description: DataTypes.TEXT
+    });
+    return Example;
+};
